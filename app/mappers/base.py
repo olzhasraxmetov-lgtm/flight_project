9d262786e72e3917392c0b21f4033f3cb7b95222
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-03-02 15:02:40 +0500

    feat: create BaseRepository, BaseService and DataMapper

---
+++ app/mappers/base.py
@@ -0,0 +1,20 @@
+from typing import TypeVar
+
+from pydantic import BaseModel
+
+from app.core.database import Base
+
+SchemaType = TypeVar('SchemaType', bound=BaseModel)
+DBModelType = TypeVar('DBModelType', bound=Base)
+
+class DataMapper:
+    db_model: type[DBModelType] = None
+    schema: type[SchemaType] = None
+
+    @classmethod
+    def map_to_domain_entity(cls, db_model):
+        return cls.schema.model_validate(db_model, from_attributes=True)
+
+    @classmethod
+    def map_to_persistent_entity(cls, data):
+        return cls.db_model(**data.model_dump())
---
+++ app/repositories/base.py
@@ -0,0 +1,67 @@
+from asyncpg import UniqueViolationError
+from pydantic import BaseModel
+from sqlalchemy import select, insert, Sequence, update, delete
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.exceptions.base import ObjectAlreadyExistException
+from app.mappers.base import DataMapper
+
+
+class BaseRepository:
+    model = None
+    mapper: DataMapper = None
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_filtered(self, *filter, **filter_by):
+        query = select(self.model).filter(*filter).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        return [self.mapper.map_to_domain_entity(model) for model in result.scalars().all()]
+
+    async def get_all(self, *args, **kwargs):
+        return await self.get_filtered()
+
+    async def get_one_or_none(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        return self.mapper.map_to_domain_entity(model)
+
+    async def get_one(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        model = result.scalar_one()
+
+        return self.mapper.map_to_domain_entity(model)
+
+    async def add(self, data: BaseModel):
+        try:
+            add_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+            result = await self.session.execute(add_stmt)
+            model = result.scalars().one()
+            return self.mapper.map_to_domain_entity(model)
+        except IntegrityError as ex:
+            if isinstance(ex.orig.__cause__, UniqueViolationError):
+                raise ObjectAlreadyExistException from ex
+            else:
+                raise ex
+
+    async def add_bulk(self, data: Sequence[BaseModel]):
+        add_stmt = insert(self.model).values([item.model_dump() for item in data])
+        await self.session.execute(add_stmt)
+
+    async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by) -> None:
+        edit_stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump(exclude_unset=exclude_unset))
+        )
+        await self.session.execute(edit_stmt)
+
+    async def delete(self, **filter_by) -> None:
+        delete_stmt = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(delete_stmt)
