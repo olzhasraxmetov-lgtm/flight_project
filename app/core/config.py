556b2dfe6e1102b65126502166ee5b1ed388d754
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-03-02 18:17:03 +0500

    chore: update Settings

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,11 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+
     APP_NAME: str = 'Flight API Project'
     APP_DESCRIPTION: str = 'Flight API Project'
     APP_VERSION: str = '0.0.1'
