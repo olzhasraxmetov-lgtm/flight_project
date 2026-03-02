15e16ead8d5578aa701b7d527080716940b4376a
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-03-02 15:12:50 +0500

    feat: add schemas for Users

---
+++ app/schemas/users.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, EmailStr
+from pydantic_extra_types.phone_numbers import PhoneNumber
+from app.helpers.users_role import UserRoleEnum
+
+class UserBase(BaseModel):
+    email: EmailStr = Field(min_length=7, max_length=30, description='Почта пользователя')
+    username: str = Field(min_length=6, max_length=15, description='Имя пользователя')
+    phone: PhoneNumber = Field(description='Телефон пользователя')
+
+class UserRequestCreate(UserBase):
+    password: str
+
+class UserCreate(UserBase):
+    hashed_password: str
+
+class UserResponse(UserBase):
+    id: int
+    role: UserRoleEnum = UserRoleEnum.USER
+    created_at: datetime
+
+class UserWithHashedPassword(UserBase):
+    hashed_password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
