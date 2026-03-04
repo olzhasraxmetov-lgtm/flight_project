1fcf78dbe0b7414b6afb10695f728543b9042be2
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-03-04 21:40:50 +0500

    fix: add await for roolback and close method in __aexit__

---
+++ app/utils/db_manager.py
@@ -0,0 +1,20 @@
+from app.repositories.users import UsersRepository
+
+class DBManager:
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+
+        self.users = UsersRepository(self.session)
+
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
