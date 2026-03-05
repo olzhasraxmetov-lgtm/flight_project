f99d6846342ba423634b5bb98a3cdbbe0889cc3c
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-03-05 20:27:53 +0500

    add MODE field to Settings and update requirements.txt

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Literal
 
 class Settings(BaseSettings):
+    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"]
+
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
