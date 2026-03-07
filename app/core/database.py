68660e74b646fbb0c8bc0e86df90f4a5fae6e0cd
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-03-07 16:36:03 +0500

    add engine_null_pool and async_session_maker_null_poll for tests

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,13 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
 async_engine = create_async_engine(url=settings.DATABASE_URL, echo=False)
+engine_null_pool = create_async_engine(url=settings.DATABASE_URL, poolclass=NullPool)
+async_session_maker_null_poll = async_sessionmaker(bind=engine_null_pool, expire_on_commit=False)
+
 AsyncSessionLocal = async_sessionmaker(
     bind=async_engine,
     expire_on_commit=False,
