a294b812cf8b4441fa35a9a92153001d37037494
Olzhas Rakhmetov <olzhasraxmetov@gmail.com>
2026-04-12 17:58:34 +0500

    add get_expired_bookings in BookingsRepository to find expired bookings

--- app/repositories/bookings.py
+++ app/repositories/bookings.py
@@ -1,7 +1,10 @@
+from datetime import datetime, timezone, timedelta
+
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 
 from app.exceptions.base import BookingNotFoundException, ObjectNotFoundException
+from app.helpers.booking_status import BookingStatus
 from app.mappers.bookings import BookingMapper
 from app.repositories.base import BaseRepository
 from app.models.bookings import BookingsORM
@@ -25,6 +28,17 @@ class BookingsRepository(BaseRepository):
         if not full_booking:
             raise ObjectNotFoundException
         return full_booking
+
+    async def get_expired_bookings(self, minutes: int = 1):
+        now = datetime.now(timezone.utc).replace(tzinfo=None)
+        threshold = now - timedelta(minutes=minutes)
+        stmt = (
+            select(self.model)
+            .where(self.model.status == BookingStatus.CREATED,
+                   self.model.created_at <= threshold)
+        )
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
 
     async def get_user_bookings(self, user_id: int):
         query = (
